10679950bebf74f2efe700e1908ad2f5c09f2b5b
Ryan Valle <ryanvalle@me.com>
2017-10-18 23:20:59 -0700

    Update about us page layout

--- app/templates/main/about.php
+++ app/templates/main/about.php
@@ -1,5 +1,6 @@
-<section class="text-blob">
+<section class="text-blob about-us">
 	<div class="constrain">
+		<div class="featured-media float-right"></div>
 		<p>We’re committed to providing high quality videos that meet our clients’ needs, and we’ve been around long enough to know what we’re doing (25+ years).</p>
 		<p>As a family owned business, we take pride in the long-term relationships we develop with our clients.  We’re honored each time we have the opportunity to tell a story for them.</p>
 		<p>Great video is all about storytelling.  Emotional impact is essential, and our videos make people laugh, cry, act, and have even saved lives.</p>
@@ -10,20 +11,20 @@
 
 <section class="what-we-do gray-block section-padding">
 	<div class="text-center"><h2>We're Full Service</h2></div>
-	<div class="column-layout constrain text-center" data-columns="3">
+	<div class="column-layout text-center constrain">
 		<div class="column">
-			<h3>Pre-Production</h3>
-			<p>The idea and planning phase, where the creative magic begins – with listening, inspiration, preparation, scripting, scouting, casting, and scheduling.</p>
+			<h3 class="reduce-margin">Pre-Production</h3>
+			<p class="text-left light text-small">The idea and planning phase, where the creative magic begins – with listening, inspiration, preparation, scripting, scouting, casting, and scheduling.</p>
 		</div>
 
 		<div class="column">
-			<h3>Production</h3>
-			<p>Shooting in studio or on location – with our talented, experienced crews who have shot all over the world, plus an award-winning director; exciting drone aerials and time lapse available; green screen in studio (we can make it look like you’re anywhere); voiceover and bilingual services.</p>
+			<h3 class="reduce-margin">Production</h3>
+			<p class="text-left light text-small">Shooting in studio or on location – with our talented, experienced crews who have shot all over the world, plus an award-winning director; exciting drone aerials and time lapse available; green screen in studio (we can make it look like you’re anywhere); voiceover and bilingual services.</p>
 		</div>
 
 		<div class="column">
-			<h3>Post-Production</h3>
-			<p>Editing, special effects, audio sweetening, graphics, using exactly the right music, formatting for your distribution needs.</p>
+			<h3 class="reduce-margin">Post-Production</h3>
+			<p class="text-left light text-small">Editing, special effects, audio sweetening, graphics, using exactly the right music, formatting for your distribution needs.</p>
 		</div>
 	</div>
 
